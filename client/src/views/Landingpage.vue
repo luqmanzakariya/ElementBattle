a7043870ce675dbd44fe0d73ad9c57a02bca76ea
adilpratama <adilpratama71@yahoo.com>
2019-08-23 10:24:44 +0700

    prepare

--- client/src/views/Landingpage.vue
+++ client/src/views/Landingpage.vue
@@ -1,57 +1,60 @@
 <template>
-<div class="m-2">
-    <b-card
-      tag="article"
-      style="width: 100%; padding: 0; margin: 0;"
-      class="mb-2"
-      :header="room.name"
-      header-text-variant="light"
-      header-bg-variant="dark"
-    >
-      <b-card-text style="padding: 0;">
-        Player: {{ room.users.length }}
-      </b-card-text>
-      <div style="display: flex; width: 100%; justify-content: space-between;">
-        <b-button variant="success" v-b-modal="room.id" style="width: 80px; padding: 5px;">Join</b-button>
-      </div>
-    </b-card>
-        <b-modal :id="room.id">
-        <b-form @submit.prevent="joinRoom(room.id)" id="formTask">
-            <b-form-input type="text" v-model="userName" placeholder="Enter Name" required></b-form-input>
-        </b-form>
-        <div slot="modal-footer">
-            <b-button variant="primary" type="submit" form="formTask">Confirm</b-button>
-        </div>
-        </b-modal>
-</div>
+    <b-container fluid>
+          <b-button variant="primary" style="padding: 5px;" v-b-modal.modal-room>CreateRoom</b-button>
+        <b-row class="dflex justify-content-center mt-5" >
+          <Cards v-for="room in rooms" :key="room.id" class="m2" :room="room"></Cards>
+        </b-row>
+          <b-modal id="modal-room">
+              <b-form @submit.prevent="createRoom" id="formTask">
+                  <b-form-input type="text" v-model="roomName" placeholder="Enter Room Name"></b-form-input>
+              </b-form>
+              <div slot="modal-footer">
+                  <b-button variant="primary" type="submit" form="formTask">Confirm</b-button>
+              </div>
+          </b-modal>
+    </b-container>
 </template>
-
 <script>
+import Cards from '../components/Cards.vue'
+import db from '../apis/firebase'
 export default {
-    props: {
-        room: Object
+    name: 'app',
+    components: {
+        Cards
     },
     data() {
         return {
-            userName: ''
+            rooms: [],
+            roomName: ''
         }
     },
-    components: {
-
-    },
     methods: {
-        joinRoom (id) {
-            this.$bvModal.hide(`${id}`)
-            const obj = {
-                id,
-                users: this.userName
-            }
-            this.$store.dispatch('addUser',obj)
-        } 
+        fetchRoom () {
+            db.collection('Rooms')
+            .onSnapshot(snapShot => {
+                this.rooms = [];
+                snapShot.forEach((doc) => {
+                    const data = doc.data()
+                    const id = doc.id
+                    this.rooms.push({
+                        id,
+                        ...data
+                    })
+                })
+            })
+        },
+        createRoom() {
+            this.$bvModal.hide('modal-room')
+            this.$store.dispatch('createRoom', this.roomName)
+        }
+    },
+    created () {
+        this.fetchRoom()
+    },
+    watch: {
+
     }
 }
 </script>
-
-<style>
-
+<style scoped>
 </style>
